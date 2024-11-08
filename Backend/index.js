7b330d398b30eb389b54cf2dc412725140a6e06a
Ganesh-Patel-U <ganesh.oficial158@gmail.com>
2024-11-08 13:12:01 +0530

    server created

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dotenv from "dotenv/config.js";
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { connectToDb } from "./Config/connectToDB.js";
 
 const PORT =process.env.PORT || 3002;
 
 const app=express();
 
 
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser())
+
+connectToDb();
 
 app.listen(PORT,()=>{
     console.log("server started at port ",PORT);
